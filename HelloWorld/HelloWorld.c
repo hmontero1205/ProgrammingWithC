2dc3f016e8f651f1438b49474705880f3ac32ce1
Hans Montero <hjm3122@columbia.edu>
2017-07-03 12:59:50 -0400

    temperature conversions

--- HelloWorld/HelloWorld.c
+++ HelloWorld/HelloWorld.c
@@ -3,5 +3,9 @@
 #include<stdio.h>
 
 main() {
-    printf("Hello World");
+    printf("Hello World! Here's some temperature conversions!\n");
+    printf("  F  |  C\n-------------\n");
+    for(int f = 0; f <= 300; f+=20) {
+    	printf("%4d | %3.1f\n", f, (5.0 / 9.0) * (f - 32));
+    }
 }
